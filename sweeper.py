278c06ec8e9eddbb9c3993d8c272e1c00eb327fd
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2021-03-18 23:12:40 -0500

    Face rec added

--- sweeper.py
+++ sweeper.py
@@ -1,16 +1,20 @@
 import time
 import requests
 from requests.api import request
-from requests.exceptions import MissingSchema, InvalidSchema
+from requests.exceptions import MissingSchema, InvalidSchema, SSLError
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException, StaleElementReferenceException
-from selenium.webdriver.chrome.options import Options
+import cv2
 
 
 
 def sweep_sweep(keyword, pics, headless) -> None:
     search = keyword.replace(' ', '%20')
     site_url = f'https://www.bing.com/images/search?q={search}&qft=+filterui:aspect-square&form=IRFLTR&first=1&tsc=ImageBasicHover'
+    
+    #cascades
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
     
     if headless == True:
         chrome_options = Options()
@@ -31,12 +35,46 @@ def sweep_sweep(keyword, pics, headless) -> None:
 
     driver.switch_to.frame('OverlayIFrame')
 
+    pic_num = 0
     for pic in range(0, pics + 1):
         try:
             images = driver.find_elements_by_tag_name('img')
             for image in images:
                 if image.get_attribute('alt') == 'See the source image' and image.get_attribute('tabindex') == '0':
-                    pic_urls.add(image.get_attribute('src'))
+                    # gray = cv2.cvtColor(face_check, cv2.COLOR_BGR2RGB)
+                    # faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
+                    # if faces != []:
+                    #     pic_urls.add(image.get_attribute('src'))
+                    url = image.get_attribute('src')
+                    
+                   
+                    if url.endswith('png') == False and url.endswith('jpg') == False and url.endswith('ImgRaw') == False and url.endswith('jpeg') == False:
+                        print('continue', url)
+                        continue
+
+                    try:
+                        request = requests.get(url)
+                    except SSLError:
+                        continue                  
+
+                    file = open('pics/' + str(pic_num) + '.png', 'wb')
+                    file.write(request.content)
+                    file.close()
+                    face_pic = cv2.imread(f'pics/{str(pic_num)}.png')
+                    gray = cv2.cvtColor(face_pic, cv2.COLOR_BGR2RGB)
+                    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
+                    
+                    
+                    time.sleep(1)
+                    pic_num += 1
+                    if len(faces) == 0 and pic_num != 0:
+                        print('no faces', url)
+                        pic_num -= 1
+                        
+
+                   
+
+
             icons = driver.find_elements_by_class_name('icon')
             for icon in icons:
                 if icon.get_attribute('title') == 'Next image result':
@@ -44,21 +82,21 @@ def sweep_sweep(keyword, pics, headless) -> None:
         except StaleElementReferenceException:
             pass
         
-    for t in enumerate(pic_urls):
-        try:
-            request = requests.get(t[1])
-            file = open('pics/' + str(t[0]) + '.png', 'wb')
-            file.write(request.content)
-            file.close()
-        except InvalidArgumentException:
-            pass
-        except MissingSchema:
-            pass
-        except IndexError:
-            pass
-        except InvalidSchema:
-            pass
-    return None
+    # for t in enumerate(pic_urls):
+    #     try:
+    #         request = requests.get(t[1])
+    #         file = open('pics/' + str(t[0]) + '.png', 'wb')
+    #         file.write(request.content)
+    #         file.close()
+    #     except InvalidArgumentException:
+    #         pass
+    #     except MissingSchema:
+    #         pass
+    #     except IndexError:
+    #         pass
+    #     except InvalidSchema:
+    #         pass
+    # return None
 
 if __name__ == '__main__':
-    sweep_sweep('', 100, False)
+    sweep_sweep('faces', 100, False)
